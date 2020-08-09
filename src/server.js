09065fea97d912d8321bd460e927637678a56a6a
Sabathick <erickick13@gmail.com>
2020-08-08 23:16:40 -0300

    Fixing some bugs about nunjucks and database

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const server= express()
+const express = require('express');
+const server = express();
 
 const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
-const nunjucks = require('nunjucks')
+const nunjucks = require('nunjucks');
 nunjucks.configure('src/views', {
-    express: server,
-    noCache: true, 
+  express: server,
+  noCache: true,
 })
 
-server.use(express.urlencoded({ extended: true }))
+server
+.use(express.urlencoded({ extended: true }))
 .use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-.post("/save-class", saveClasses)
-.listen(5500)
+.post("/save-classes", saveClasses)
+
+.listen(5500);
